0e7b0168a1d025d52e37826d87e7f9168ca25197
dawmthedev <dominiqmartinez13@gmail.com>
2024-12-11 16:47:11 -0800

    Update router and homepage configuration for custom domain

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
